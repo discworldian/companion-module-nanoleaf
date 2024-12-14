181305b048f4dbbca5dac0cefb44799eb8e31ad1
Cayce Hengeveld <cayce.hengeveld@vivacitylabs.com>
2024-12-14 21:30:11 +0000

    Change things

--- src/actions.ts
+++ src/actions.ts
@@ -3,7 +3,7 @@ import type { ModuleInstance } from './main.ts'
 export function UpdateActions(self: ModuleInstance): void {
 	self.setActionDefinitions({
 		ping: {
-			name: 'Ping',
+			name: 'Forest',
 			options: [
 				{
 					id: 'ip',
@@ -21,22 +21,44 @@ export function UpdateActions(self: ModuleInstance): void {
 				{ id: 'auth_token', type: 'textinput', label: 'API auth token' },
 			],
 			callback: async (event) => {
-
 				let ip = event.options.ip
 				let port = event.options.port
 				let auth_token = event.options.auth_token
 
-				const api_url = `https://${ip}:${port}/api/v1/${auth_token}`
+				const api_url = `https://${ip}:${port}/api/v1/${auth_token}/effects`
 
 				try {
 					const response = await fetch(
 						new Request(api_url, {
 							method: 'PUT',
-							body: JSON.stringify({ "select": "Forest" }),
+							body: JSON.stringify({ select: 'Blaze' }),
 							headers: {
 								'Content-Type': 'application/json',
 							},
-							
+						}),
+					)
+					if (!response.ok) {
+						throw new Error(`Response status: ${response.status}`)
+					}
+
+					const json = await response.json()
+					console.log(json)
+				} catch (error) {
+					console.error(error.message)
+				}
+
+				await new Promise(f => setTimeout(f, 1000 * 10));
+
+
+
+				try {
+					const response = await fetch(
+						new Request(api_url, {
+							method: 'PUT',
+							body: JSON.stringify({ select: 'Forest' }),
+							headers: {
+								'Content-Type': 'application/json',
+							},
 						}),
 					)
 					if (!response.ok) {
